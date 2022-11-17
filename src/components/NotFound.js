49382ea6d24e8c9d874cc2cbd5f80eb6d8752b93
Yesenia Sosa Gonzalez <gonzalez.yesenia0621@gmail.com>
2022-11-16 23:23:46 -0800

    Update

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,11 +1,10 @@
 import React from "react";
-import Robot from "./Images/Robot.png"
 
 const NotFound = (props) => {
     return (
         <li class="not-found">
             <h3>Sorry! No Results Found</h3>
-            <img src={Robot} alt="Robot" />
+            <img src={require('./Robot.png')} alt='' />
             <p>You search did not return any results. Please try again.</p>
         </li>
     );
