3ce0fbb2d2261464f46c41bbbbaa7120febff0f3
Yesenia Sosa Gonzalez <gonzalez.yesenia0621@gmail.com>
2022-11-16 19:29:54 -0800

    Update

--- src/App.js
+++ src/App.js
@@ -3,21 +3,34 @@ import { Route, Routes } from "react-router-dom";
 import axios from "axios";
 import apiKey from "./config";
 
+//Creating the app through components 
+import Nav from "./components/Nav";
+import SearchForm from "./components/SearchForm";
+import PhotoContainer from "./components/PhotoContainer";
+import NotFound from "./components/NotFound";
+
+
 function App() {
   const [photo, setPhoto] = useState();
+  const 
 
   useEffect(() => {
-  axios.get('')
-    .then(function (response) {
-      // handle success
-    })
-    .catch(function (error) {
-      // handle error
-      console.log(error);
-    });
+
   }, []);
 
-  return <div>App</div>;
+  axios.get('')
+  .then(function (response) {
+    // handle success
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  });
+
+
+  return (
+    <div>App</div>
+  );
 }
 
 export default App;
--- src/components/Photos.js
+++ src/components/Photos.js
@@ -6,7 +6,7 @@ const Photos = (props) => {
         <li>
             <img src="https://farm5.staticflickr.com/4334/37032996241_4c16a9b530.jpg" alt="" />
         </li>
-    )
+    );
 };
 
 export default Photos;
