5a5ab59e33996b9d857c09cdb536f03ad3dd7dbe
Yesenia Sosa Gonzalez <gonzalez.yesenia0621@gmail.com>
2022-11-16 17:45:54 -0800

    Add files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
-//import { Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 //import axios from 'axios';
-
+import apiKey from './config';
 
 function App() {
   const [photo, setPhoto] = useState();
