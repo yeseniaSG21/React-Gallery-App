5ae4990cb5722a637ba87b32f63dcd33303fdded
Yesenia Sosa Gonzalez <gonzalez.yesenia0621@gmail.com>
2022-11-21 19:15:47 -0800

    Update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 //Import our require dependencies 
 import React, { useState, useEffect } from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import axios from "axios";
 import apiKey from "./config";
 
@@ -11,15 +11,20 @@ import PhotoContainer from "./components/PhotoContainer";
 import NotFound from "./components/NotFound";
 
 
-function App() {
-  const [photo, setPhoto] = useState();
-  const [] = useState();
+function App(props) {
+  const [photo, setPhoto] = useState([]);
+  const [sunsets, setSunsets] = useState([]);
+  const [mountains, setMountains] = useState([]);
+  const [elephants, setElephants] = useState([]);
 
   useEffect(() => {
-
+    performSearch();
+    performSearch("sunsets");
+    performSearch("mountains");
+    performSearch("elepahnts");
   }, []);
 
-  axios.get('')
+  axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${tags}&per_page=24&format=json&nojsoncallback=1`)
   .then(function (response) {
     // handle success
   })
@@ -30,7 +35,18 @@ function App() {
 
 
   return (
-    <div>App</div>
+    <div className="container">
+      <SearchForm onSearch={performSearch} />
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Navigate to="/sunsets" />} />
+        <Route path="/sunsets" element={<PhotoContainer data={sunsets} />} />
+        <Route path="/mountains" element={<PhotoContainer data={mountains} />} />
+        <Route path="/elephants" element={<PhotoContainer data={elephants} />} />
+        <Route path="/search/:keyword" element={<PhotoContainer data={photos} />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
   );
 }
 
