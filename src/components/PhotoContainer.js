b850ddf118af6a8b96c7c429e8c6c121b178350b
Yesenia Sosa Gonzalez <gonzalez.yesenia0621@gmail.com>
2022-11-21 21:22:23 -0800

    Update

--- src/components/PhotoContainer.js
+++ src/components/PhotoContainer.js
@@ -1,27 +1,11 @@
 import React, { useEffect } from "react";
-import { useLocation, useParams } from "react-router-dom";
 import Photos from "./Photos";
-import NotFound from "./NotFound";
 
-const PhotoContainer = ({ photoData, performSearch }) => {
-    const location = useLocation();
-    let params = useParams();
+//Photo Container where photo data (from API) can be managed
+const PhotoContainer = (props) => {
+    const photoData = props.data;
 
-    //
-    //Reference: https://www.w3schools.com/jsref/prop_loc_pathname.asp
-    useEffect(() => {
-        if (params.tag) {
-            performSearch(params.tag);
-        } else {
-            const pathname = location.pathname.substring(1);
-            performSearch(pathname);
-        }
-    }, [params.tags, location.pathname]);
-
-    let photoList;
-
-    if (photoData.length > 0) {
-        photoList = photoData.map((photo) => (
+    let photoList = photoData.map((photo) => (
             <Photos 
                 id={photo.id}
                 key={photo.id}
@@ -30,9 +14,6 @@ const PhotoContainer = ({ photoData, performSearch }) => {
                 secret={photo.secret}
             />
         ));
-    } else {
-        photoList = <NotFound />;
-    }
 
     return (
         <div className="photo-container">
