c1a951b67a3fbf0d7a13a5ea16fe9c0c3664741a
Yesenia Sosa Gonzalez <gonzalez.yesenia0621@gmail.com>
2022-11-21 23:15:54 -0800

    Update

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -2,15 +2,15 @@ import React from "react";
 
 //Added a Robot Image to the Not Found Message: credit to https://www.freepik.com
 const mystye = {
-    width: '500px',
+    width: '600px',
     height: '400px'
 };
 
 const NotFound = (props) => {
     return (
         <li className="not-found">
-            <h2>Sorry! No Results Found</h2>
-            <p>Your search did not return any results. Please try again or return home.</p>
+            <h2>Sorry! No Results Found!</h2>
+            <h3>Please try a new search or refresh browser.</h3>
             <img style={mystye} src={require('./Robot.png')} alt='' />
         </li>
     );
